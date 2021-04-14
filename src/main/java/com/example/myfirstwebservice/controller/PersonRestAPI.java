5892c0ceca93f8601062ee7d435e75ad391d46a2
aristotlemiranda <aristotlemiranda@gmail.com>
2021-04-14 22:41:07 +0800

    Added html file, index.html to demo rest api via Javascript

--- src/main/java/com/example/myfirstwebservice/controller/PersonRestAPI.java
+++ src/main/java/com/example/myfirstwebservice/controller/PersonRestAPI.java
@@ -3,6 +3,7 @@ package com.example.myfirstwebservice.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/record")
 @Api(value = "Person Rest Controller")
